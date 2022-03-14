d6d1a7ea991df93038ed43169f66021ae043e765
Nemanja <nemanja.jovic195@gmail.com>
2022-03-13 17:37:38 -0700

    code cleanup

--- qstation_kurs/src/App.js
+++ qstation_kurs/src/App.js
@@ -1,14 +1,12 @@
-import './App.css';
-import Persons from './components/Persons';
+import Persons from './components/Persons'
 
 export const App = () => {
-
   return (
     <div className="App">
       <h1>App component</h1>
       <Persons />
     </div>
-  );
+  )
 }
 
 // export default App;
